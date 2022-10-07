0fc36eaa7eae80803c75c7d436e1e3007db51f37
Sergio k <valodya.kirik@gmail.com>
2022-10-07 12:36:34 +0300

    changed variables names

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,11 +9,13 @@ const asString = (a, b) => {
 
   if (stringA > stringB) {
     return 1;
-  } else if (stringA === stringB) {
-    return 0;
-  } else {
-    return -1;
   }
+
+  if (stringA === stringB) {
+    return 0;
+  }
+
+  return -1;
 };
 
 function applyCustomSort() {
@@ -23,13 +25,13 @@ function applyCustomSort() {
     do {
       count = 0;
 
-      for (let i = 1; i < this.length; i++) {
-        const previous = this[i - 1];
-        const current = this[i];
+      for (let index = 1; index < this.length; index++) {
+        const previous = this[index - 1];
+        const current = this[index];
 
         if (compareFunction(previous, current) > 0) {
-          this[i - 1] = current;
-          this[i] = previous;
+          this[index - 1] = current;
+          this[index] = previous;
           count++;
         }
       }
